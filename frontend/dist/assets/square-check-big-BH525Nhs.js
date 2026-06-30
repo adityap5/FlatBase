f5a26af5ac1fc598b04af3044f79a7d990ed99ab
adityap5 <aditya29pippal@gmail.com>
2026-06-30 13:56:08 +0530

    feat: add full-stack user registration and authentication flow with UI components

---
+++ frontend/dist/assets/square-check-big-BH525Nhs.js
@@ -0,0 +1,7 @@
+import{c as a}from"./index-CjNwHBcx.js";
+/**
+ * @license lucide-react v0.510.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=a("dollar-sign",[["line",{x1:"12",x2:"12",y1:"2",y2:"22",key:"7eqyqh"}],["path",{d:"M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6",key:"1b0p4s"}]]),s=a("square-check-big",[["path",{d:"M21 10.5V19a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h12.5",key:"1uzm8b"}],["path",{d:"m9 11 3 3L22 4",key:"1pflzl"}]]);export{e as D,s as S};
